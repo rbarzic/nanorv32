e2cee666c97c64067bc9ab59174380a0916aa706
Ronan Barzic <rbarzic@gmail.com>
2016-04-07 00:10:40 +0200

    Update...

--- common/generators/port_mux.py
+++ common/generators/port_mux.py
@@ -64,11 +64,19 @@ cfg['pad']['control']['din'] = {
 }
 
 cfg['pad']['control']['oe'] = {
-    'desc' : "Pad outpule enable",
+    'desc' : "Pad output enable",
     'dir' : 'ip2pad',
     'default' : 0,
     'polarity' : 'active_high'
 }
+
+cfg['pad']['control']['ie'] = {
+    'desc' : "Pad input enable",
+    'dir' : 'ip2pad',
+    'default' : 0,
+    'polarity' : 'active_high'
+}
+
 
 
 
@@ -137,7 +145,7 @@ pin_muxing['USBPAD@dm']= {
     'func_A' : 'usb/dm',
 }
 
-pin_muxing['USBPAD@dm']= {
+pin_muxing['USBPAD@dp']= {
     'func_A' : 'usb/dp',
 }
 
@@ -150,7 +158,7 @@ padring['P0']['order'] = 1
 padring['P1']['type'] = 'stdpad3v3p180n'
 padring['P1']['order'] = 10
 
-padring['USBPAD']['type'] = 'stdpad3v3p180n'
+padring['USBPAD']['type'] = 'usbpad3v3p180n'
 padring['USBPAD']['order'] = 15
 
 
@@ -161,52 +169,52 @@ padring['USBPAD']['order'] = 15
 iocells['stdpad3v3p180n']['type'] = 'io'
 
 # Mapping of IOCELL pin to generic control signal
-iocells['stdpad3v3180n']['pads']['pad']['dout'] = {
+iocells['stdpad3v3p180n']['pads']['pad']['dout'] = {
     'signal' : 'i18dout',
     'polarity' : 'active_high'
 }
-iocells['stdpad3v3180n']['pads']['pad']['din'] = {
+iocells['stdpad3v3p180n']['pads']['pad']['din'] = {
     'signal' : 'o18din',
     'polarity' : 'active_high'
 }
 
-iocells['stdpad3v3180n']['pads']['pad']['oe'] = {
+iocells['stdpad3v3p180n']['pads']['pad']['oe'] = {
     'signal' : 'i18oe',
     'polarity' : 'active_high'
 }
 
 
-# Standard I/O pad
+# Non standard  I/O cell, with multiple pads
 # definition
 iocells['usbpad3v3p180n']['type'] = 'custom'
 
 # Mapping of IOCELL pin to generic control signal
 
 # DM pin
-iocells['usbpad3v3180n']['pads']['dm']['dout'] = {
+iocells['usbpad3v3p180n']['pads']['dm']['dout'] = {
     'signal' : 'i18dout[0]',
     'polarity' : 'active_high'
 }
-iocells['usbpad3v3180n']['pads']['dm']['din'] = {
+iocells['usbpad3v3p180n']['pads']['dm']['din'] = {
     'signal' : 'o18din[0]',
     'polarity' : 'active_high'
 }
 
-iocells['stdpad3v3180n']['pads']['dm']['oe'] = {
+iocells['usbpad3v3p180n']['pads']['dm']['oe'] = {
     'signal' : 'i18oe[0]',
     'polarity' : 'active_high'
 }
 # DP pin
-iocells['usbpad3v3180n']['pads']['dp']['dout'] = {
+iocells['usbpad3v3p180n']['pads']['dp']['dout'] = {
     'signal' : 'i18dout[1]',
     'polarity' : 'active_high'
 }
-iocells['usbpad3v3180n']['pads']['dp']['din'] = {
+iocells['usbpad3v3p180n']['pads']['dp']['din'] = {
     'signal' : 'o18din[1]',
     'polarity' : 'active_high'
 }
 
-iocells['usbpad3v3180n']['pads']['dp']['oe'] = {
+iocells['usbpad3v3p180n']['pads']['dp']['oe'] = {
     'signal' : 'i18oe[1]',
     'polarity' : 'active_high'
 }
@@ -227,54 +235,53 @@ def type_of_instance(inst):
     else:
         print "-E- instance {} not found !".format(inst)
 
-def get_signal_for_iocell(iocell, pad, func):
-    "Return signal and polarity for the fucntion (dout,din, oe...) for the pad of the  iocell"
+def get_signal_for_iocell(iocells, pad, func):
+    "Return signal and polarity for the function (dout,din, oe...) for the pad of the  iocell"
     pass
 
-def get pad_names(inst_pad):
+def get_iocell_type(padring,cell):
+    return padring[cell]['type']
+
+def get_pad_names(padring, iocells, inst_pad):
     "Return an array of all pad names for this instance - array will have one element for std pads"
-    pass
+    t = get_iocell_type(padring,inst_pad)
+    print "Type = {}".format(t)
+    return [p for p in iocells[t]['pads'].keys()]
 
-#for pad  in pads.keys():
-#    print "-I Port {}".format(pad)
-#    # get functions used by this pad, sorted by decreasing priority
-#    ordered_func = [f for f in pads[pad]['pmux']]
-#    pp.pprint(ordered_func)
-#    control_signals = get_control_signals(cfg)
-#    pp.pprint(control_signals)
-print "-I- Let's go"
-for pad,pmux in  pin_muxing.items():
-    print "*"*80
-    ordered_func = [f for f in pads[pad]['pmux']] # FIXME - this is not ordered
-    pp.pprint(ordered_func)
-    control_signals = get_control_signals(cfg)
-    pp.pprint(control_signals)
-    print "-I- Pad {}".format(pad)
-    # pp.pprint(get_pad_functions(pads,pad))
-    pad_funcs = [f for f in ordered_func if f in get_pad_functions(pads,pad)]
-    if '@' in pad:
-        # we have a couple pad/instance name
-        pass
+def get_iocell_instance(pinmux_elt):
+    return pinmux_elt.split('@')[0]
+
+def get_iocell_pad(iocells, iocell_type, pinmux_elt):
+    if '@' in pinmux_elt:
+        # Fixme : check if this pad exists for this cell
+        return pinmux_elt.split('@')[1]
     else:
-        # pad = instance name
-        # we need to get the pad name for this instance
+        # FIXME : Add an extra check to be sure there is only one pad
+        return iocells[iocell_type]['pads'].keys()[0]
 
-    for func in pad_funcs:
-        inst = pmux['pmux'][func].split('/')[0]
-        ip        = type_of_instance( inst)
-        sig_group = pmux['pmux'][func].split('/')[1]
-        print "-I-     Function {} - Inst : {}({}) Signal group : {}".format(func,inst,ip,sig_group)
-        # get each signals
-        # all_signals = ips[ip]['sig_grp'][sig_group]
-        # TODO : check correctness of sigmal group
-        if ip in ips:
-            all_signals = ips[ip]['sig_grp'][sig_group]
-            for ctrl,sig in all_signals.items():
-                print "-I          signal {} for controlling <{}>".format(sig,ctrl)
-        else:
-            print "-E- Unknown IP : {}".format(ip)
+def get_control_signals_for_pad(iocells, iocell, pad):
+    print "-E- iocell : " + iocell
+    print "-E- pad : " + pad
+    return iocells[iocell]['pads'][pad].keys()
 
-        # Now found the signals supported by the current pad of the IO cell
-        #get_signal_for_iocell()
 
-            # print "-I-       Signal".format(sig)
+def get_iocell_type_from_instance(inst):
+    return padring[inst]['type']
+
+print "Pad list for {} : {}".format('P0',str(get_pad_names(padring, iocells, 'P0')))
+print "Pad list for {} : {}".format('P1',str(get_pad_names(padring, iocells, 'P1')))
+print "Pad list for {} : {}".format('USBPAD',str(get_pad_names(padring, iocells, 'USBPAD')))
+
+
+for pm, val  in pin_muxing.items():
+    print "Pin muxing for {}".format(pm)
+    iocell_inst = get_iocell_instance(pm)
+    iocell_type = get_iocell_type_from_instance(iocell_inst)
+    current_pad = get_iocell_pad(iocells, iocell_type, pm)
+    print "Type of iocell is {} (using {}) ".format( iocell_type, iocell_inst)
+    print "Pad  of iocell is {}  ".format( get_iocell_pad(iocells, iocell_type, pm))
+
+    print "List of expected control signals {}  ".format( get_control_signals(cfg))
+
+    # get list of control signal this pad support
+    print "List of actual control signals {}  ".format( get_control_signals_for_pad(iocells,iocell_type, current_pad))
