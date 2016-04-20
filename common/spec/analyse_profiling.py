cbb85c9ecb66293d7adabaa9788e7cc6ca8769ed
Barzic, Ronan <rbarzic@gmail.com>
2016-04-20 15:11:16 +0200

    Add plot support

--- common/spec/analyse_profiling.py
+++ common/spec/analyse_profiling.py
@@ -7,6 +7,11 @@ import pprint as pp
 import pickle
 from operator import itemgetter
 
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def get_args():
     """
     Get command line arguments
@@ -16,7 +21,28 @@ Analyse results provided by the --profile option of the Nanorv32 simulator
                    """)
     parser.add_argument('--profile', action='store', dest='profile',
                         help='Profiling data from the simulator (Pickle format)', default="")
+
+    parser.add_argument('--plot', action='store_true', dest='plot',
+                        help='Generate plots for the 10 most time consuming functions', default=False)
     return parser.parse_args()
+
+
+def plot_functions(func_dat):
+    """Generate a plot regarding split between functions"""
+    l = zip(*func_dat)
+    func_names = l[0]
+    cycles     = l[1]
+    print "="*80
+    pp.pprint(func_names)
+    pp.pprint(cycles)
+    print "="*80
+    y_pos = np.arange(len(func_names))
+    plt.barh(y_pos, cycles, align='center', alpha=0.4)
+    plt.yticks(y_pos, func_names)
+    plt.xlabel('Cycles')
+    plt.title('How fast do you want to go today?')
+    plt.show()
+
 
 if __name__ == '__main__':
 
@@ -37,6 +63,7 @@ if __name__ == '__main__':
         print "-I Done - Cycles : {}".format(total_cycle)
 
         pp.pprint(sorted_pairs)
+        plot_functions(sorted_pairs[0:10])
         # Display the Top ten contributors
         for f,c in sorted_pairs[0:10]:
             print "- Function  {} : {}%".format(f,(c*100/total_cycle))
