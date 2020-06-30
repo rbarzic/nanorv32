b8fe596d659accff5864b6ee21484c31c867b42a
Barzic, Ronan <rbarzic@gmail.com>
2020-06-30 23:04:45 +0200

    Update makehex.py script to handle recent toolchain

--- common/scripts/makehex.py
+++ common/scripts/makehex.py
@@ -6,7 +6,7 @@
 # distribute this software, either in source code form or as a compiled
 # binary, for any purpose, commercial or non-commercial, and by any
 # means.
-
+import sys
 from sys import argv
 
 binfile = argv[1]
@@ -16,12 +16,28 @@ with open(binfile, "rb") as f:
     bindata = f.read()
 
 assert len(bindata) < 4*nwords
-assert len(bindata) % 4 == 0
+remaining_bytes = len(bindata) % 4
+
+
+
+    
+# assert len(bindata) % 4 == 0, "Number of byte {l} not divisible by 4 ".format(l=len(bindata))
 
 for i in range(nwords):
     if i < len(bindata) // 4:
         w = bindata[4*i : 4*i+4]
-        print("%02x%02x%02x%02x" % (w[3], w[2], w[1], w[0]))
+        print("%02x%02x%02x%02x" % (w[3], w[2], w[1], w[0]))    
+    elif i ==  len(bindata) // 4:
+        if remaining_bytes == 3:
+            w = bindata[4*i : 4*i+3]
+            print("%02x%02x%02x%02x" % (0, w[2], w[1], w[0]))    
+        elif remaining_bytes == 2:
+            w = bindata[4*i : 4*i+2]
+            print("%02x%02x%02x%02x" % (0, 0, w[1], w[0]))    
+        elif remaining_bytes == 1:
+            w = bindata[4*i : 4*i+1]
+            print("%02x%02x%02x%02x" % (0, 0, 0, w[0]))    
+
     else:
         print("0")
 
