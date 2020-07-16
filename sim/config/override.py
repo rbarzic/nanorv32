20ffe3f2b21dd1f86d1ab63539dd2f9a15cdb334
Ronan BARZIC <ronan.barzic@onio.com>
2020-07-16 10:25:17 +0200

    Update Python simulation scripts to Python 3 - part II

--- sim/config/override.py
+++ sim/config/override.py
@@ -1,9 +1,9 @@
 # empty for now
 if test_is_a_file and (test_dir == "../riscv-tests/isa/rv32ui"):
     if verbosity >0:
-        print "-I- From override.py : test {} is a file".format(test)
-        print "-I- From override.py : test_name {} ".format(test_name)
-        print "-I- From override.py : test_dir <{}> ".format(test_dir)
+        print("-I- From override.py : test {} is a file".format(test))
+        print("-I- From override.py : test_name {} ".format(test_name))
+        print("-I- From override.py : test_dir <{}> ".format(test_dir))
     cfg['c_compiler']['extra_c_sources'] = "$(TEST_DIR)/" + test_name + ".S"
     cfg['c_compiler']['extra_incdirs'] = " -I$(TOP)/common/include "
     cfg['c_compiler']['extra_defines'] = " -DTEST_FUNC_NAME={} ".format(test_name)
@@ -14,7 +14,7 @@ if test_is_a_file and (test_dir == "../riscv-tests/isa/rv32ui"):
 
 # Opus codec on Nanorv32
 if test == "../../opus":
-    print "-I- Special setup for the opus codec"
+    print("-I- Special setup for the opus codec")
     cfg['c_compiler']['extra_c_sources'] = "$(TEST_DIR)/" + "nanorv32/main.c "
     cfg['c_compiler']['extra_c_sources'] += "$(TEST_DIR)/" + "nanorv32/retarget.c "
     cfg['c_compiler']['extra_c_sources'] += "$(TEST_DIR)/"  + "src/opus.c "
